b536744bfd7c5c55869dc5a4565bdd538943daed
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-11-07 20:42:56 +0000

    Adding soft deletes & Updating doc comments

--- src/Models/User.php
+++ src/Models/User.php
@@ -3,11 +3,13 @@
 use Arcanedev\LaravelAuth\Bases\Model;
 use Arcanedev\LaravelAuth\Contracts\User as UserContract;
 use Arcanedev\LaravelAuth\Traits\AuthUserRelationships;
+use Carbon\Carbon;
 use Illuminate\Auth\Authenticatable;
 use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\Access\Authorizable;
 
 /**
@@ -15,6 +17,21 @@ use Illuminate\Foundation\Auth\Access\Authorizable;
  *
  * @package  Arcanedev\LaravelAuth\Models
  * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
+ *
+ * @property  int     id
+ * @property  string  username
+ * @property  string  first_name
+ * @property  string  last_name
+ * @property  string  email
+ * @property  string  password
+ * @property  string  remember_token
+ * @property  bool    active
+ * @property  bool    confirmed
+ * @property  string  confirmation_code
+ * @property  Carbon  confirmed_at
+ * @property  Carbon  created_at
+ * @property  Carbon  updated_at
+ * @property  Carbon  deleted_at
  */
 class User
     extends Model
@@ -24,7 +41,7 @@ class User
      |  Traits
      | ------------------------------------------------------------------------------------------------
      */
-    use Authenticatable, Authorizable, AuthUserRelationships, CanResetPassword;
+    use Authenticatable, Authorizable, AuthUserRelationships, CanResetPassword, SoftDeletes;
 
     /* ------------------------------------------------------------------------------------------------
      |  Properties
@@ -46,6 +63,15 @@ class User
      */
     protected $hidden   = [
         'password', 'remember_token', 'confirmation_code',
+    ];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'confirmed_at', 'deleted_at'
     ];
 
     /* ------------------------------------------------------------------------------------------------
