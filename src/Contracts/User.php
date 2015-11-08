1a1103c090cb85ef6700f1e50f3548cbc493ee09
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-11-08 18:55:05 +0000

    Updating the User model

--- src/Contracts/User.php
+++ src/Contracts/User.php
@@ -5,6 +5,21 @@
  *
  * @package   Arcanedev\LaravelAuth\Contracts
  * @author    ARCANEDEV <arcanedev.maroc@gmail.com>
+ *
+ * @property  int             id
+ * @property  string          username
+ * @property  string          first_name
+ * @property  string          last_name
+ * @property  string          email
+ * @property  string          password
+ * @property  string          remember_token
+ * @property  bool            is_active
+ * @property  bool            is_confirmed       (Optional)
+ * @property  string          confirmation_code  (Optional)
+ * @property  \Carbon\Carbon  confirmed_at       (Optional)
+ * @property  \Carbon\Carbon  created_at
+ * @property  \Carbon\Carbon  updated_at
+ * @property  \Carbon\Carbon  deleted_at
  */
 interface User
 {
@@ -18,4 +33,17 @@ interface User
      * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
      */
     public function roles();
+
+    /* ------------------------------------------------------------------------------------------------
+     |  CRUD Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Update the user model in the database.
+     *
+     * @param  array  $attributes
+     *
+     * @return bool|int
+     */
+    public function update(array $attributes = []);
 }
--- src/Models/User.php
+++ src/Models/User.php
@@ -2,13 +2,14 @@
 
 use Arcanedev\LaravelAuth\Bases\Model;
 use Arcanedev\LaravelAuth\Contracts\User as UserContract;
+use Arcanedev\LaravelAuth\Services\UserConfirmator;
 use Arcanedev\LaravelAuth\Traits\AuthUserRelationships;
-use Carbon\Carbon;
 use Illuminate\Auth\Authenticatable;
 use Illuminate\Auth\Passwords\CanResetPassword;
-use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\Access\Authorizable;
 
@@ -18,20 +19,7 @@ use Illuminate\Foundation\Auth\Access\Authorizable;
  * @package  Arcanedev\LaravelAuth\Models
  * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
  *
- * @property  int     id
- * @property  string  username
- * @property  string  first_name
- * @property  string  last_name
- * @property  string  email
- * @property  string  password
- * @property  string  remember_token
- * @property  bool    active
- * @property  bool    confirmed
- * @property  string  confirmation_code
- * @property  Carbon  confirmed_at
- * @property  Carbon  created_at
- * @property  Carbon  updated_at
- * @property  Carbon  deleted_at
+ * @method   Builder  unconfirmed(string $code)
  */
 class User
     extends Model
@@ -66,6 +54,16 @@ class User
     ];
 
     /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'is_active'    => 'boolean',
+        'is_confirmed' => 'boolean',
+    ];
+
+    /**
      * The attributes that should be mutated to dates.
      *
      * @var array
@@ -88,5 +86,129 @@ class User
         $this->setTable(config('laravel-auth.users.table', 'users'));
 
         parent::__construct($attributes);
+    }
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Scopes
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Scope unconfirmed users by code.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  string                                 $code
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeUnconfirmed(Builder $query, $code)
+    {
+        return $query->where('is_confirmed', false)
+            ->where('confirmation_code', $code)
+            ->whereNull('confirmed_at');
+    }
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Getters & Setters
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Set the password attribute.
+     *
+     * @param  string  $password
+     */
+    public function setPasswordAttribute($password)
+    {
+        $this->attributes['password'] = bcrypt($password);
+    }
+
+    /* ------------------------------------------------------------------------------------------------
+     |  CRUD Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Activate the user.
+     *
+     * @return bool
+     */
+    public function activate()
+    {
+        return $this->switchActive(true);
+    }
+
+    /**
+     * Deactivate the user.
+     *
+     * @return bool
+     */
+    public function deactivate()
+    {
+        return $this->switchActive(false);
+    }
+
+    /**
+     * Activate/deactivate the user.
+     *
+     * @param  bool  $active
+     *
+     * @return bool
+     */
+    protected function switchActive($active)
+    {
+        $this->is_active = boolval($active);
+
+        return $this->save();
+    }
+
+    /**
+     * Confirm the unconfirmed user account by confirmation code.
+     *
+     * @param  string  $code
+     *
+     * @return \Arcanedev\LaravelAuth\Models\User
+     *
+     * @throws \Illuminate\Database\Eloquent\ModelNotFoundException
+     */
+    public function findUnconfirmed($code)
+    {
+        /** @var self $user */
+        return self::unconfirmed($code)->firstOrFail();
+    }
+
+    /**
+     * Confirm the new user account.
+     *
+     * @param  string  $code
+     *
+     * @return \Arcanedev\LaravelAuth\Models\User
+     */
+    public function confirm($code)
+    {
+        $user = $this->findUnconfirmed($code);
+
+        return (new UserConfirmator)->confirm($user);
+    }
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Check Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Check if user has an activated account.
+     *
+     * @return bool
+     */
+    public function isActive()
+    {
+        return $this->is_active;
+    }
+
+    /**
+     * Check if user has a confirmed account.
+     *
+     * @return bool
+     */
+    public function isConfirmed()
+    {
+        return $this->is_confirmed;
     }
 }
