b0d439dfbbcf65054b631460de22ed77b3bf621c
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-11-08 18:54:12 +0000

    Adding UserConfirmator

---
+++ src/Services/UserConfirmator.php
@@ -0,0 +1,87 @@
+<?php namespace Arcanedev\LaravelAuth\Services;
+
+use Arcanedev\LaravelAuth\Contracts\User;
+use Illuminate\Database\Schema\Blueprint;
+
+/**
+ * Class     UserConfirmator
+ *
+ * @package  Arcanedev\LaravelAuth\Services
+ * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+class UserConfirmator
+{
+    /* ------------------------------------------------------------------------------------------------
+     |  Main Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Generate the confirmation code.
+     *
+     * @return string
+     */
+    public static function generateCode()
+    {
+        return str_random(self::getLength());
+    }
+
+    /**
+     * Confirm user account.
+     *
+     * @param  User  $user
+     *
+     * @return User
+     */
+    public function confirm(User $user)
+    {
+        event('auth.user.confirming', compact('user'));
+
+        $user->update([
+            'confirmed'         => true,
+            'confirmation_code' => null,
+            'confirmed_at'      => \Carbon\Carbon::now(),
+        ]);
+
+        event('auth.user.confirmed', compact('user'));
+
+        return $user;
+    }
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Other Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Check if the confirmator is enabled.
+     *
+     * @return bool
+     */
+    public static function isEnabled()
+    {
+        return config('laravel-auth.user-confirmation.enabled', false);
+    }
+
+    /**
+     * Get confirmation code length.
+     *
+     * @return int
+     */
+    public static function getLength()
+    {
+        return config('laravel-auth.user-confirmation.length', 30);
+    }
+
+    /**
+     * Add confirmation columns.
+     *
+     * @param  Blueprint  $table
+     */
+    public static function addColumns(Blueprint $table)
+    {
+        if (self::isEnabled()) {
+            $table->boolean('is_confirmed')->default(0);
+            $table->string('confirmation_code', self::getLength())->nullable();
+            $table->timestamp('confirmed_at')->nullable();
+        }
+    }
+}
