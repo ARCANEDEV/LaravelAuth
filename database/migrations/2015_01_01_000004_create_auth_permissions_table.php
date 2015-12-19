26c0e767b2df89325df6231d2a103bc40a4f86d8
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-12-19 15:13:40 +0000

    Updating the migrations

---
+++ database/migrations/2015_01_01_000004_create_auth_permissions_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Arcanedev\LaravelAuth\Bases\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+/**
+ * Class     CreatePermissionsTable
+ *
+ * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+class CreateAuthPermissionsTable extends Migration
+{
+    /* ------------------------------------------------------------------------------------------------
+     |  Constructor
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Make a migration instance.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->setTable(config('laravel-auth.permissions.table'));
+    }
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Main Functions
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::connection($this->connection)->create($this->table, function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('group_id')->unsigned()->default(0);
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->string('slug');
+            $table->timestamps();
+
+            $table->unique('slug');
+        });
+    }
+}
