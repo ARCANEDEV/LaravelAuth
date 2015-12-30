d205a5dfafe6f86228221f9dd11edb4930676ae5
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-12-30 18:40:17 +0000

    Fixing the indentation

--- config/laravel-auth.php
+++ config/laravel-auth.php
@@ -60,14 +60,14 @@ return [
      | ------------------------------------------------------------------------------------------------
      */
     'seeds'              => [
-    'users' => [
-        [
-            'username'   => 'admin',
-            'email'      => env('ADMIN_USER_EMAIL', 'admin@email.com'),
-            'password'   => env('ADMIN_USER_PASSWORD', 'password'),
+        'users' => [
+            [
+                'username'   => 'admin',
+                'email'      => env('ADMIN_USER_EMAIL', 'admin@email.com'),
+                'password'   => env('ADMIN_USER_PASSWORD', 'password'),
+            ],
         ],
     ],
-],
 
     /* ------------------------------------------------------------------------------------------------
      |  Other Stuff
